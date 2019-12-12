22b33eed3fe5a95f1353c63d95b7d0ef7285c054
swapniltathe <swapniltathe.hcl@gmail.com>
2019-12-12 18:44:46 +0530

    IBM Laptop files

--- script.py
+++ script.py
@@ -1,15 +1,2 @@
-from math import gcd
-_t_ = int(input())
-if 1 <= _t_ <= 15:
-    for t in range(_t_):
-        n, a, b, k = map(int, input().split())
-        if 1 <= k <= n <= 1000000000000000000:
-            if 1 <= a <= 1000000000 and 1 <= b <= 1000000000:
-                u = (a * b) / gcd(a, b)
-                appy = n//a
-                chef = n//b
-                comn = n//u
-                if (appy + chef) - (2 * comn) >= k:
-                    print("Win")
-                else:
-                    print("Lose")
+print("Hello!")
+
