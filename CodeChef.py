eda29dfac2d9af4ed650107d744765a9b0d1adbe
HomePC <swapniltathe.hcl@gmail.com>
2019-12-15 21:06:32 +0530

    15-12-2019

--- CodeChef.py
+++ CodeChef.py
@@ -263,3 +263,45 @@ for __ in range(int(input())):
                 break
     print(cnt)
 ############################################
+# https://www.codechef.com/DEC19B/problems/BINXOR
+def tostring(lst):
+    return ''.join(lst)
+
+
+def permute(a, l, r):
+    if l == r:
+        b_perm.append(tostring(a))
+        # print(tostring(a))
+    else:
+        for i in range(l, r+1):
+            a[l], a[i] = a[i], a[l]
+            permute(a, l+1, r)
+            a[l], a[i] = a[i], a[l]
+
+
+for __ in range(int(input())):
+    n = int(input())
+    a = [_ for _ in input()]
+    b = [_ for _ in input()]
+    a_perm = []
+    b_perm = []
+    res = []
+    out = []
+    if n == len(a) and n == len(b):
+        # print(a, b)
+        permute(a, 0, n-1)
+        a_perm = b_perm.copy()
+        b_perm.clear()
+        permute(b, 0, n-1)
+        # print(a_perm)
+        # print(b_perm)
+        for m in (int(m, 2) for m in a_perm):
+            for j in (int(j, 2) for j in b_perm):
+                res.append(m ^ j)
+        # print(len(res))
+        for k in range(len(res)):
+            if res[k] not in out:
+                # print(res[k])
+                out.append(res[k])
+        print(len(out))
+############################################
--- all.py
+++ all.py
@@ -1965,6 +1965,35 @@ if __name__ == "__main__":
 #    finally:
         # End Program
 ########################################
+# permutations 
+# Python program to print all permutations with
+# duplicates allowed
+
+
+def tostring(lst):
+    return ''.join(lst)
+
+
+# Function to print permutations of string
+# This function takes three parameters:
+# 1. String
+# 2. Starting index of the string
+# 3. Ending index of the string.
+def permute(a, l, r):
+    if l == r:
+        print(tostring(a))
+    else:
+        for i in range(l, r+1):
+            a[l], a[i] = a[i], a[l]
+            permute(a, l+1, r)
+            a[l], a[i] = a[i], a[l] # backtrack
+
+
+# Driver program to test the above function
+string = "ABC"
+n = len(string)
+a = list(string)
+permute(a, 0, n-1)
 
 
 
